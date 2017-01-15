da4dfdc667e4ebac5edc309e96fd807430107bf9
Unknown <cristiancapisizu@yahoo.com>
2017-01-15 13:13:07 +0200

    1st commit by Cristian

--- app/src/main/java/com/upb/fils/freds/FirstScreen.java
+++ app/src/main/java/com/upb/fils/freds/FirstScreen.java
@@ -2,6 +2,8 @@ package com.upb.fils.freds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FirstScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+
+    }
+
+    public void goToMainMenu(View view){
+        Intent intent = new Intent(this, .class);
     }
 }
